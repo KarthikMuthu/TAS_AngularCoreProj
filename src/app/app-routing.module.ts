7813183c81be3943954867f5b8e853442f1d5651
Saimuthukarthik <connectmuthu28@gmail.com>
2020-03-18 15:28:20 +0530

    router module change

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './loginpage/login.component';
 
-
 const routes: Routes = [
   // { path: 'demo', component: DemoComponent },
     { path: '', component: LoginComponent }
