f87e87a128efbd5f8a07c8a71b2452cb51c3406b
suryavalli026 <suryavalli@6solve.com>
2020-03-19 12:44:26 +0530

    change in app-module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,6 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './loginpage/login.component';
 import { FormsModule,ReactiveFormsModule } from '@angular/forms';
 
-
 @NgModule({
   declarations: [
     AppComponent,
